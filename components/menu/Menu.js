ff4d616cc7281ef6511e94713eee35bb17f3e105
Artem Valkov <valkov.artyom@gmail.com>
2020-12-04 01:27:14 +0200

    menu upgrade

--- components/menu/Menu.js
+++ components/menu/Menu.js
@@ -1,6 +1,13 @@
 import {useSelector, useDispatch} from "react-redux";
 import Link from 'next/link'
+import Router from 'next/router'
 import openMenu from "../../redux/actionCreators/menuActionCreator";
+import projects from '../../styles/img/projects.svg'
+
+const RedirectPage = (page) => {
+    Router.push(page)
+}
+
 
 export default function Menu() {
 
@@ -8,10 +15,34 @@ export default function Menu() {
     const changeMenuState = useDispatch()
     return (
         <div className={menuState ? "menu_open" : "menu_close"}>
-            <div hidden={!menuState} className="menu_item" onClick={() => changeMenuState(openMenu(false))}> <Link href='/login'> Sign-in </Link></div>
-            <div hidden={!menuState} className="menu_item"> Home </div>
-            <div hidden={!menuState} className="menu_item"> Home </div>
-            <div hidden={!menuState} className="menu_item"> Home </div>
+            <div className={!menuState ? "menu_item" : "menu_item-hide"}
+                 onClick={() => changeMenuState(openMenu(false))}>
+                <div
+                    onClick={() => RedirectPage('/login')} className="menu_item-link">
+                    <p>Home</p>
+                    {/*<img className="menu_item-img" src={projects} alt="Home"/>*/}
+                    <object data={projects} type="image/svg+xml" className="menu_item-img"></object>
+                </div>
+            </div>
+            <div className={!menuState ? "menu_item" : "menu_item-hide"}
+                 onClick={() => changeMenuState(openMenu(false))}>
+                <div
+                    onClick={() => RedirectPage('/login')} className="menu_item-link">
+                    <p>Projects</p>
+                    <img className="menu_item-img" src={projects} alt="Home"/>
+
+                </div>
+            </div>
+            <div className={!menuState ? "menu_item" : "menu_item-hide"}
+                 onClick={() => changeMenuState(openMenu(false))} >
+                <div
+                    onClick={() => RedirectPage('/admin')}className="menu_item-link">
+                    <p>Contacts</p>
+                    <img className="menu_item-img" src={projects} alt="Home" />
+
+                </div>
+            </div>
+
         </div>
     )
 }
--- components/menu/MenuButton.js
+++ components/menu/MenuButton.js
@@ -5,12 +5,12 @@ export default function MenuButton({menuState, hideMenu, func}) {
     return (
         <>
             {!menuState
-                ? <label hidden={hideMenu} onClick={func} className="burger">
+                ? <label hidden={hideMenu} onClick={func} className={!menuState ?"burger" : "burger-opened"}>
                     <span className="bar top"></span>
                     <span className="bar middle"></span>
                     <span className="bar bottom"></span>
                 </label>
-                : <label hidden={hideMenu} onClick={func} className="burger">
+                : <label hidden={hideMenu} onClick={func} className={!menuState ?"burger" : "burger-opened"}>
                     <span className="bar top-tr"></span>
                     <span className="bar middle-tr"></span>
                     <span className="bar bottom-tr"></span>
