6b37bf39e85ad712f2ceb0ad13c76583ed5269b9
Artem Valkov <valkov.artyom@gmail.com>
2020-12-04 21:43:48 +0200

    hide text

--- pages/index.js
+++ pages/index.js
@@ -34,7 +34,7 @@ export default function Home() {
                     />
 
                         <div className="container">
-                            Hi! I will show my project!
+                            git s
                             <Main/>
 
                             <Menu/>
