8d33bd7dda1598d847df2d2377b75c45eacbe0ed
Artem Valkov <valkov.artyom@gmail.com>
2020-12-27 22:29:57 +0200

    fix

--- components/Loader.js
+++ components/Loader.js
@@ -9,7 +9,7 @@ export default function CustomLoader() {
                 color="#00BFFF"
                 height={60}
                 width={60}
-                timeout={3000} //3 secs
+                timeout={0} //3 secs
             />
         </div>
     )
