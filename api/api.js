7b26a7554fbadfb8825dfabf3b9d9d8570dc6972
Artem Valkov <valkov.artyom.fs@gmail.com>
2020-12-27 22:50:07 +0200

    fix

--- api/api.js
+++ api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
  const api = axios.create({
-    baseURL: 'http://back-env.eba-mur2bxyw.eu-central-1.elasticbeanstalk.com/'
+    baseURL: 'https://back-env.eba-mur2bxyw.eu-central-1.elasticbeanstalk.com/'
 })
 
 export default api
